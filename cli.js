9c2557d1fb5f7cdc51512fbf0ca4b030334a738a
alestor123 <alestoraldous@gmail.com>
2021-01-07 19:25:07 +0530

    added main code

---
+++ cli.js
@@ -0,0 +1,5 @@
+var placeholder = require('./App'),
+options = require('minimist')(process.argv.slice(2));
+(async () => {
+placeholder(options.t || options.text,options.font || options.f,options.s || options.size)
+})();
